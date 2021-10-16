a4e76fd66fdc26813560ebc359d2454bb035ed7b
Jatin Gupta <201951073@iiitvadodara.ac.in>
2021-10-16 17:35:38 +0530

    admin api added

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
 const PORT = process.env.PORT || 9000;
-
+app.use("/admin", require("./routes/admin"));
 mongoose.connect(process.env.mongoDBUrl, () => {
   console.log(`Server is Up!`);
   app.listen(PORT);
